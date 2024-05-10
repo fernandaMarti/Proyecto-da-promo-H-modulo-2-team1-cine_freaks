c9941633ba275741fbddcdd5a3ebdc4b18112d42
Silvia Piñel Fañanás <99440874+spinelf@users.noreply.github.com>
2024-05-10 16:01:51 +0200

    Create Script_BBDD.sql

---
+++ Script_BBDD.sql
@@ -0,0 +1,65 @@
+
+
+CREATE SCHEMA CinemExtract;
+
+USE CinemExtract;
+
+CREATE TABLE MoviesDataset (
+id_pelicula INT NOT NULL,
+titulo_pelicula VARCHAR (100),
+tipo_pelicula VARCHAR (45),
+genero_pelicula VARCHAR (45),
+anno_estreno INT,
+mes_estreno VARCHAR (45),
+PRIMARY KEY (id_registro)
+);
+
+CREATE TABLE premios (
+id_premio INT AUTO_INCREMENT NOT NULL,
+genero_pelicula VARCHAR (45),
+fecha_ceremonia DATE,
+mejor_pelicula VARCHAR (255),
+mejor_director VARCHAR (100),
+mejor_actor VARCHAR (100),
+mejor_actriz VARCHAR (100),
+PRIMARY KEY (id_premio),
+
+);
+
+CREATE TABLE actores (
+id_actor INT,
+nombre_actor VARCHAR (100),
+anno_nacimiento INT,
+conocido VARCHAR (45),
+que_hace VARCHAR (45),
+premios INT,
+
+);
+
+CREATE TABLE detalles_peliculas (
+id_detalle_peli INT AUTO_INCREMENT NOT NULL,
+id_pelicula VARCHAR(20)
+nombre_pelicula VARCHAR (100),
+genero_pelicula VARCHAR (45),
+argumento VARCHAR (2000),
+duracion VARCHAR (45),
+guionistas VARCHAR(255),
+directores VARCHAR (255),
+puntuacion_imdb VARCHAR (45),
+puntuacion_rotten VARCHAR (45),
+PRIMARY KEY (id_detalle_peli),
+FOREIGN KEY (id_pelicula) REFERENCES MoviesDataset (id_pelicula)
+);
+
+CREATE TABLE int_pelis_actores (
+id_actor INT
+id_pelicula VAARCHAR(20)
+PRIMARY KEY (id_actor,id_pelicula),
+FOREIGN KEY (id_actor) REFERENCES actores (id_actor),
+FOREIGN KEY (id_pelicula ) REFERENCES MoviesDataset (id_pelicula)  
+);
+
+
+
+
+  
